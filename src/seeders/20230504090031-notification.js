357b5aa1816a8840ed45298f768e56d2c87dacdc
Angelizines <angelita_j20@mhs.istts.ac.id>
2023-05-04 16:33:54 +0700

    transaction & notification

---
+++ src/seeders/20230504090031-notification.js
@@ -0,0 +1,29 @@
+'use strict';
+const { faker } = require('@faker-js/faker');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const notifications = [];
+    for (let i = 1; i <= 20; i++) {
+      notifications.push({
+        description: "This is a dummy notification!",
+        id_user:faker.datatype.number({ min: 1, max: 10 }),
+        id_accomodation:faker.datatype.number({ min: 1, max: 10 }),
+        status:1,
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+    }
+    await queryInterface.bulkInsert('notifications',notifications, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('notifications', null, {});
+  }
+};
