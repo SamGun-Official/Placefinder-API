5fc654a1dcafcc7a7137ae0ece953ba527257dd6
Clarissa Gracienne <clarissagracia.cg@gmail.com>
2023-05-12 14:27:52 +0700

    Update 7_20230511011155-dtrans.js

--- src/seeders/7_20230511011155-dtrans.js
+++ src/seeders/7_20230511011155-dtrans.js
@@ -1,4 +1,5 @@
 'use strict';
+const db = require('../config/sequelize');
 
 const { faker } = require('@faker-js/faker');
 /** @type {import('sequelize-cli').Migration} */
@@ -9,10 +10,47 @@ module.exports = {
     const d_trans = [];
   
       for (let i = 0; i < 20; i++) {
+        
+        let usages = [];
+        let id_htrans = faker.datatype.number({ min: 1, max: 10 });
+
+        let[result, metadata] = await db.sequelize.query("SELECT * FROM H_TRANS WHERE id = ?",{
+          replacements: [id_htrans]
+        });
+
+        let h_trans = result[0];
+
+        [result, metadata] = await db.sequelize.query("SELECT * FROM USAGES WHERE id_user = ?", {
+          replacements: [h_trans.id_user]
+        });
+
+        usages = result;
+
+        do{
+         
+          id_htrans = faker.datatype.number({ min: 1, max: 10 });
+
+          [result, metadata] = await db.sequelize.query("SELECT * FROM H_TRANS WHERE id = ?",{
+            replacements: [id_htrans]
+          });
+  
+          h_trans = result[0];
+  
+          [result, metadata] = await db.sequelize.query("SELECT * FROM USAGES WHERE id_user = ?", {
+            replacements: [h_trans.id_user]
+          });
+
+          usages = result;
+        }while(usages.length==0);
+
+        var randomIndex = Math.floor(Math.random() * usages.length);
+
+        const usage = usages[randomIndex];
+
         const newD_trans = {
-          id_htrans: faker.datatype.number({ min: 1, max: 10 }),
-          id_usage: faker.datatype.number({ min: 1, max: 10 }),
-          subtotal: faker.datatype.number({ min: 100, max: 500 }),
+          id_htrans: id_htrans,
+          id_usage: usage.id,
+          subtotal: usage.subtotal,
           status: faker.datatype.number({ min: 0, max: 3 }),
           created_at: new Date(),
           updated_at: new Date()
